0a82a63fa8b2b991749c73a5940bc1fb483acf2e
AlanMonterroza <amonterr10@cuc.edu.co>
2024-11-20 22:23:00 -0500

    CRUD-Listar Plato version completa

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -4,14 +4,12 @@ export const Inicio = () => {
       <div className="pt-5 d-flex justify-content-center">
         <div className="col-md-6">
           <div className="h-100 p-5 text-bg-dark rounded-3">
-            <h2>Change the background</h2>
+            <h2>¡Bienvenidos!</h2>
             <p>
-              Swap the background-color utility and add a `.text-*` color
-              utility to mix up the jumbotron look. Then, mix and match with
-              additional component themes and more.
+            Bienvenidos a Juanlocuras, donde cada plato es una celebración de los sabores auténticos de Colombia. Déjate llevar por la magia de nuestras recetas tradicionales, llenas de pasión, historias y el inconfundible sazón que une a todo un país. ¡Descubre, disfruta y saborea lo mejor de nuestra tierra!
             </p>
             <button className="btn btn-outline-light" type="button">
-              Example button
+              Pide ahora
             </button>
           </div>
         </div>
--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,8 +1,12 @@
 export const Acerca = () => {
   return (
     <>
-      Soy el acerca <br />
-      El de la descripcion...
+     <div className="container-fluid py-5">
+         <h1 className="display-5 fw-bold">Acerca de nosotros</h1>
+         <p className="col-md-8 fs-4">Somos un restaurante nuevo e independiente nos complace dar buenas experiencias a todos nuestros clientes. Gracias por elegirnos siempre.</p>
+         <button className="btn btn-primary btn-lg" type="button">Más informacion</button>
+       </div>
+    
     </>
   );
 };
